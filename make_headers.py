db0d2f02fd8df4ba516f74b6db1b924ad2e73709
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-03 19:43:22 +0000

    Adding numeric_owner as keyword arguement

--- make_headers.py
+++ make_headers.py
@@ -62,7 +62,7 @@ def _generate_headers(ver: str, verbose: bool):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar, path=dst)
